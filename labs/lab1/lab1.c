71599e965b05b0b0cae147ad85103ec8886b316f
Nayrasoy <naybetan@ucm.es>
2025-09-29 12:58:28 +0200

    Laboratorio 1

---
+++ labs/lab1/lab1.c
@@ -0,0 +1,77 @@
+/*-------------------------------------------------------------------
+**
+**  Fichero:
+**    lab1.c  25/8/2016
+**
+**    (c) J.M. Mendias
+**    Programación de Sistemas y Dispositivos
+**    Facultad de Informática. Universidad Complutense de Madrid
+**
+**  Propósito:
+**    Test del laboratorio 1
+**
+**  Notas de diseño:
+**    Presupone que todo el SoC ha sido previamente configurado
+**    por el programa residente en ROM que se ejecuta tras reset
+**
+**-----------------------------------------------------------------*/
+
+#define SEGS (*(volatile unsigned char *) 0x2140000 )
+
+const unsigned char hex2segs[16] = {0x12, 0x9F, 0x31, 0x15,0x9C, 0x54, 0x50, 0x1F, 0x10, 0x14, 0x18, 0xD0, 0x72, 0x91, 0x70, 0x78 };
+// 									 0,	   1, 	 2,    3, 	 4,   5,    6,    7,    8,    9,    A,    B,    C,    D,    E,    F
+// 0- 0b00010010
+// 1- 0b10011111
+// 2- 0b00110001
+// 7- 0b00011111
+// 8- 0b00010000
+// d- 0b10010001
+
+void main(void) 
+{
+
+    unsigned char i;
+    unsigned int j;
+
+    SEGS = 0xff;
+    while( 1 )
+        for( i=0; i<16; i++ )
+        {
+            for( j=0; j<300000; j++ );
+                SEGS = hex2segs[i];
+        }
+}
+
+
+/*
+
+void main(void) 
+{
+
+    unsigned char i;
+    unsigned int j;
+
+    SEGS = 0xff;
+    while( 1 )
+        for( i=0; i<16; i++ )
+        {
+            for( j=0; j<300000; j++ );
+            switch( i ){
+                case 0x00:
+                    SEGS = 0x12; // 0b00010010;
+                    break;
+                case 0x01:
+                    SEGS = 0xCF; // 0b110011111
+                    break;
+                ...
+                case 0x0f:
+                    SEGS = ...;
+                    break;
+                default:
+                    SEGS = ...;
+                    break;
+            }
+        }
+
+}
+*/
