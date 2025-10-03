986c2f5d61746d59c5c08f5e0947d0379a255f66
Nayrasoy <naybetan@ucm.es>
2025-10-03 13:28:29 +0200

    lab2 hecho y el 3 en camino

---
+++ labs/lab3/Debug/lab3.d
@@ -0,0 +1,11 @@
+lab3.d lab3.o: ../lab3.c ../../BSP/include/common_types.h \
+ ../../BSP/include/system.h ../../BSP/include/segs.h \
+ ../../BSP/include/leds.h
+
+../../BSP/include/common_types.h:
+
+../../BSP/include/system.h:
+
+../../BSP/include/segs.h:
+
+../../BSP/include/leds.h:
