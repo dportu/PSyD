eb2b96d194d19e9549b115c885778883ea0976d1
Nayrasoy <naybetan@ucm.es>
2025-10-13 12:29:20 +0200

    ficheros lab4

---
+++ labs/lab4/Debug/lab4.d
@@ -0,0 +1,8 @@
+lab4.d lab4.o: ../lab4.c ..\..\BSP\include/common_types.h \
+ ..\..\BSP\include/system.h ..\..\BSP\include/uart.h
+
+..\..\BSP\include/common_types.h:
+
+..\..\BSP\include/system.h:
+
+..\..\BSP\include/uart.h:
